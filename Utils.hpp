d13e78e63213574f959415c40b245c72a2e32b48
Mickael Juin <114703612+m-juin@users.noreply.github.com>
2025-11-23 14:30:41 +0100

    Change color constants to inline variables
    Changed color constants to inline variables for working with multiple .cpp.

--- Utils.hpp
+++ Utils.hpp
@@ -5,11 +5,11 @@ namespace LoggerLib::Utils
 {
     namespace Color
     {
-        const char *NONE = "\033[0m";
-        const char *RED = "\033[31;1m";
-        const char *WHITE = "\033[37;1m";
-        const char *GREEN = "\033[32;1m";
-        const char *YELLOW = "\033[33;1m";
+        inline const char *NONE = "\033[0m";
+        inline const char *RED = "\033[31;1m";
+        inline const char *WHITE = "\033[37;1m";
+        inline const char *GREEN = "\033[32;1m";
+        inline const char *YELLOW = "\033[33;1m";
     } // namespace Color
 
 }
